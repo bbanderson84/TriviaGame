dfdc4a4516c06f6bbda4c295443483227e2e0550
bbanderson84 <bbanderson84@gmail.com>
2019-01-24 00:52:48 -0700

    recent update

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+var userSCore = 0;
+
+
+$(document).on("click", "#button", function () {
+
+    document.getElementById("start_game").style.visibility = "visible";
+    document.getElementById("button").style.visibility = "hidden";
+});
+
